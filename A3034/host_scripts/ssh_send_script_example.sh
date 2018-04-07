9cd8f84d607047c55dac30000f28d6410ca64b47
Michael Collins <collins@opensourceinstruments.com>
2018-04-06 20:43:05 -0400

    added EV argument for streaming example

--- A3034/host_scripts/ssh_send_script_example.sh
+++ A3034/host_scripts/ssh_send_script_example.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Exammple of using ssh to call a script
-ssh -i /Users/collins/OSI/VBT/A3034/ssh_keys/id_rsa pi@10.0.0.234 'bash -s' < /Users/collins/OSI/VBT/A3034/scripts/stream_1232.sh
+ssh -i /Users/collins/OSI/VBT/A3034/ssh_keys/id_rsa pi@10.0.0.234 'bash -s 2' < /Users/collins/OSI/VBT/A3034/scripts/stream_1232.sh
 
