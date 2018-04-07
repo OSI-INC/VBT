f2c71bda879c4dff8ff2c0d69289b24b3bf07a2c
Michael Collins <collins@opensourceinstruments.com>
2018-04-06 20:29:32 -0400

    typo

--- A3034/scripts/stream_1232.sh
+++ A3034/scripts/stream_1232.sh
@@ -3,4 +3,4 @@
 
 # Requires an argument from -10 to 10 for exposure compensation. Positive values are brighter. "2" or "3" is recommended for black mice.
 
-screen -dm | raspivid -t 0 -fl -w 1640 -h 1232 -ev $1 -ih -fps 20 -l -o tcp://0.0.0.0:2222
+screen -dm raspivid -t 0 -fl -w 1640 -h 1232 -ev $1 -ih -fps 20 -l -o tcp://0.0.0.0:2222
